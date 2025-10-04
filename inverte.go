bd7f50464dd52e3b7e2c80fd0950526c1225d41f
João Victor <elietejvc@hotmail.com>
2025-10-04 00:14:26 -0300

    invertendo sem append

--- inverte.go
+++ inverte.go
@@ -4,14 +4,32 @@ import "fmt"
 
 func main(){
 	inverterArray(1, 2, 3 ,4)
+
+	arr := []int{1,2, 3, 4}
+	inverteArray2(arr)
 }
 
 func inverterArray(a, b, c, d int) {
 	array := [4]int{a, b, c, d}
 	inv := []int{}
-	for i := len(array) - 1; i >= 0; i--{   
+	for i := len(array) - 1; i >= 0; i--{     
 		inv = append(inv, array[i])
 	}
 
 	fmt.Println(inv)
+}
+
+  //sem append
+func inverteArray2(arr [] int) []int{
+	start := 0
+	end := len(arr) - 1
+	
+
+	for start < end {
+		arr[start], arr[end] = arr[end], arr[start]
+		start++
+		end--
+	}
+	fmt.Println(arr)
+	return  arr
 }
